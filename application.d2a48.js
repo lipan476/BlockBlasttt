125ee2d3dbfb651af10405e090857723677bef93
lipan <670339228@qq.com>
2025-05-06 17:03:09 +0800

    修改代码

--- application.d2a48.js
+++ application.d2a48.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.82a62.json';
+          this.settingsPath = 'src/settings.a764c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
