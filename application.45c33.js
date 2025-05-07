c3b2ce19b531137e47021e20df2edc9dae834ded
lipan <670339228@qq.com>
2025-05-07 10:39:42 +0800

    提交代码

--- application.45c33.js
+++ application.45c33.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.84b5d.json';
+          this.settingsPath = 'src/settings.ca551.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.dae24.js
+++ index.dae24.js
@@ -1,4 +1,4 @@
-System.register(["./application.71f64.js"], function (_export, _context) {
+System.register(["./application.45c33.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
