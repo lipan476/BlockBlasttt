9e9f702591625401506c47a0dd59f2bfe466c903
lipan <670339228@qq.com>
2025-05-07 13:46:34 +0800

    asdasdasdas12

--- index.f236a.js
+++ index.f236a.js
@@ -1,4 +1,4 @@
-System.register(["./application.28050.js"], function (_export, _context) {
+System.register(["./application.e86d9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
