25b693e65073f8d010b2d798b17ec454a258edd3
lipan <670339228@qq.com>
2025-05-07 15:10:49 +0800

    123123

--- index.083ff.js
+++ index.083ff.js
@@ -1,4 +1,4 @@
-System.register(["./application.82438.js"], function (_export, _context) {
+System.register(["./application.f0cd5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
