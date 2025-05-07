0c1062e1eaf8d7f02441aa2d02a93e7c0acb981f
lipan <670339228@qq.com>
2025-05-07 16:43:40 +0800

    修复代码

--- index.5a215.js
+++ index.5a215.js
@@ -1,4 +1,4 @@
-System.register(["./application.f0cd5.js"], function (_export, _context) {
+System.register(["./application.52933.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
