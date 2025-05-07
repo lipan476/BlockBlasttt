53b3de00684eb93c4b541388cbfe2132e07c475b
lipan <670339228@qq.com>
2025-05-07 12:20:03 +0800

    提交代码

--- application.ace32.js
+++ application.ace32.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.3e4bf.json';
+          this.settingsPath = 'src/settings.182fd.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.ee9f0.js
+++ index.ee9f0.js
@@ -1,4 +1,4 @@
-System.register(["./application.5fa08.js"], function (_export, _context) {
+System.register(["./application.ace32.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
