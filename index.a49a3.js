9a25d3d5c1814d4bc14b624d9be449496e313086
lipan <670339228@qq.com>
2025-05-07 16:54:09 +0800

    添加代码

--- index.a49a3.js
+++ index.a49a3.js
@@ -1,4 +1,4 @@
-System.register(["./application.0ef7c.js"], function (_export, _context) {
+System.register(["./application.58d88.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
