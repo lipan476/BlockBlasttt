65643dd6db248a7a76d2592f986641a79994ee91
lipan <670339228@qq.com>
2025-05-07 14:50:37 +0800

    修复代码

--- index.5e6f9.js
+++ index.5e6f9.js
@@ -1,4 +1,4 @@
-System.register(["./application.f052b.js"], function (_export, _context) {
+System.register(["./application.82438.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
