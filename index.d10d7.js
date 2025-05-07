443aba71c2b29e72ec6cf234fdbadc0f71f08e7a
lipan <670339228@qq.com>
2025-05-07 11:37:21 +0800

    提交代码

--- index.d10d7.js
+++ index.d10d7.js
@@ -1,4 +1,4 @@
-System.register(["./application.1ed03.js"], function (_export, _context) {
+System.register(["./application.5fa08.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
