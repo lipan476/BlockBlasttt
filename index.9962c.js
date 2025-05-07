0549fd7389015a931aa32c798e74322e2fc604f6
lipan <670339228@qq.com>
2025-05-07 12:25:00 +0800

    asdasdasd

--- index.9962c.js
+++ index.9962c.js
@@ -1,4 +1,4 @@
-System.register(["./application.ace32.js"], function (_export, _context) {
+System.register(["./application.81b09.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
