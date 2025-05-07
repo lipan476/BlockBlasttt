adb5586463e6f0c6a5759129db173d741d99c1aa
lipan <670339228@qq.com>
2025-05-07 13:32:44 +0800

    修复代码

--- index.9f6f5.js
+++ index.9f6f5.js
@@ -1,4 +1,4 @@
-System.register(["./application.80563.js"], function (_export, _context) {
+System.register(["./application.28050.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
