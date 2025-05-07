81f71c01b8f5f31e112792d7b0923fbb48d27c35
lipan <670339228@qq.com>
2025-05-07 16:45:44 +0800

    asdasdj提交代码

--- index.f499e.js
+++ index.f499e.js
@@ -1,4 +1,4 @@
-System.register(["./application.52933.js"], function (_export, _context) {
+System.register(["./application.0ef7c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
