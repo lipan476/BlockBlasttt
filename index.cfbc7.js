a55469453636d17b3c6cdd949f84d2890e38d4b6
lipan <670339228@qq.com>
2025-05-07 14:15:53 +0800

    dasdasd133

--- index.cfbc7.js
+++ index.cfbc7.js
@@ -1,4 +1,4 @@
-System.register(["./application.e86d9.js"], function (_export, _context) {
+System.register(["./application.d469e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
