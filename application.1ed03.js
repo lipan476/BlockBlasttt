6b5cfa73af585b21a709e02912d6baecc0674c91
lipan <670339228@qq.com>
2025-05-07 11:26:38 +0800

    提交代码

--- application.1ed03.js
+++ application.1ed03.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.ef5b4.json';
+          this.settingsPath = 'src/settings.0dcc6.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
