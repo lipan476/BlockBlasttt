be62405c336074e55d183c2cdaf93f1ff9f93fbb
lipan <670339228@qq.com>
2025-05-08 12:21:19 +0800

    提交代码

--- index.586d0.js
+++ index.586d0.js
@@ -1,4 +1,4 @@
-System.register(["./application.830d9.js"], function (_export, _context) {
+System.register(["./application.faf3e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
